1182cb64373295e626bd9e77174a087a6ae6e70e
mufhidaturrahma <117068447+mufhidaturrahma@users.noreply.github.com>
2022-10-31 15:35:10 +0700

    Add files via upload

---
+++ MODUL 1/src/Database.java
@@ -0,0 +1,17 @@
+public class Database {
+    String insert;
+    String menu;
+    String search;
+
+    public String insertMenu(){
+        return insert;
+    }
+
+    public String showMenu(){
+        return menu;
+    }
+
+    public String searchMenu(){
+        return search;
+    }
+}
---
+++ MODUL 1/src/Menu.java
@@ -0,0 +1,18 @@
+public class Menu {
+    String name;
+    String category;
+    String price;
+
+
+    public String name(){
+        return name;
+    }
+
+    public String category(){
+        return category;
+    }
+
+    public String price(){
+        return price;
+    }
+}
